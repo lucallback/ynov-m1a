a47ba8a6aeadf2d9d7680936a3e3c0cbc4c3b4c3
Lucas SAISSI <lucas.saissi@gmail.com>
2024-04-30 10:13:19 +0200

    feat: tf one rg

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-# 1. Specify the version of the AzureRM Provider to use
 terraform {
   required_providers {
     azurerm = {
@@ -8,7 +7,6 @@ terraform {
   }
 }
 
-# 2. Configure the AzureRM Provider
 provider "azurerm" {
   features {}
 }
@@ -16,10 +14,8 @@ provider "azurerm" {
 resource "random_pet" "rg-pet" {
 }
 
-# 3. Create a resource group
 resource "azurerm_resource_group" "rgtest" {
   name     = "rg-${random_pet.rg-pet.id}-001"
   location = "France Central"
-  tags     = {env = "test"}
+  tags     = {env = "test", team = "developers"}
 }
-
