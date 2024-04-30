fdb7579fe557799cfa3ea3a8fcdc1a80e054784b
Lucas SAISSI <lucas.saissi@gmail.com>
2024-04-30 13:54:09 +0200

    feat: remote tfstate in Azure

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "=3.0.1"
     }
   }
+    backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "m1atfstate15222"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
